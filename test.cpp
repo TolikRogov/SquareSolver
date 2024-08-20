d55e7c3ff4b3dab88f1b5fa8782cb116572134c3
TolikRogov <tolik_rogov@bk.ru>
2024-08-20 19:43:15 +0300

    enum nRoots

--- test.cpp
+++ test.cpp
@@ -1,31 +1,36 @@
 #include <stdio.h>
 #include <math.h>
+#include <ctype.h>
 
-const int SS_INF_ROOTS = -1;
 const double SS_MIN_VAL = 10e-6;
 
-int Solver(double a, double b, double c, double *x1, double *x2);
+typedef enum {
+    INF_ROOTS = -1,
+    NO_ROOTS,
+    ONE_ROOT,
+    TWO_ROOTS
+} nRoots;
+
+nRoots Solver(double a, double b, double c, double *x1, double *x2);
 int NearZero(double n);
-int SolveLine(double b, double c, double *x1, double *x2);
+nRoots SolveLine(double b, double c, double *x1, double *x2);
 int PrintAnswer(int nRoots, double *x1, double *x2);
 int GetDiscriminant(double a, double b, double c);
-int SolveSquare(double a, double b, double c, double *x1, double *x2);
-int Insert(double *coeff);
+nRoots SolveSquare(double a, double b, double c, double *x1, double *x2);
+int Insert(double *coeff, int cnt);
 
 int main() {
 
     double a = 0, b = 0, c = 0;
     double x1 = 0, x2 = 0;
 
-    Insert(&a);
-    Insert(&b);
-    Insert(&c);
+    Insert(&a, 1);
+    Insert(&b, 2);
+    Insert(&c, 3);
 
-    //  дальше читать про enum, сделать enum c количество корней
+    nRoots check = Solver(a, b, c, &x1, &x2);
 
-    int nRoots = Solver(a, b, c, &x1, &x2);
-
-    PrintAnswer(nRoots, &x1, &x2);
+    PrintAnswer(check, &x1, &x2);
 
     return 0;
 }
@@ -34,39 +39,39 @@ int NearZero(double n) {
     return fabs(n) < SS_MIN_VAL;
 }
 
-int SolveLine(double b, double c, double *x1, double *x2) {
+nRoots SolveLine(double b, double c, double *x1, double *x2) {
     if (NearZero(b)) {
-        return (NearZero(c))? SS_INF_ROOTS : 0;
+        return (NearZero(c))? INF_ROOTS : NO_ROOTS;
     }
     else {
         *x1 = -c / b;
         *x2 = -c / b;
-        return 1;
+        return ONE_ROOT;
     }
 }
 
-int PrintAnswer(int nRoots, double *x1, double *x2) {
-    switch(nRoots) {
-        case 0: {
+int PrintAnswer(nRoots check, double *x1, double *x2) {
+    switch(check) {
+        case NO_ROOTS: {
             printf("No roots \n");
             break;
         }
-        case 1: {
+        case ONE_ROOT: {
             printf("One root \n");
             printf("x = %lg \n", *x1);
             break;
         }
-        case 2: {
+        case TWO_ROOTS: {
             printf("Two roots \n");
             printf("x1 = %lg \n x2 = %lg \n", *x1, *x2);
             break;
         }
-        case SS_INF_ROOTS: {
+        case INF_ROOTS: {
             printf("Infinite amount of roots \n");
             break;
         }
         default: {
-            printf("main(): ERROR: nRoots = %d \n", nRoots);
+            printf("main(): ERROR: amount of roots is not defined");
             return 1;
         }
     }
@@ -77,24 +82,24 @@ int GetDiscriminant(double a, double b, double c) {
     return b * b - 4 * a * c;
 }
 
-int SolveSquare(double a, double b, double c, double *x1, double *x2) {
+nRoots SolveSquare(double a, double b, double c, double *x1, double *x2) {
     double d = GetDiscriminant(a, b, c);
 
     if(NearZero(d)) {
         *x1 = *x2 = -b / ( 2 * a );
-        return 1;
+        return ONE_ROOT;
     }
     else if(d > 0) {
         double q = sqrt(d);
         *x1 = ( -b + q ) / (2 * a);
         *x2 = ( -b - q ) / (2 * a);
-        return 2;
+        return TWO_ROOTS;
     }
-    return 0;
+    return NO_ROOTS;
 }
 
-int Solver(double a, double b, double c, double *x1, double *x2) {
-    int n = 0;
+nRoots Solver(double a, double b, double c, double *x1, double *x2) {
+    nRoots n;
     if (NearZero(a)) {
         n = SolveLine(b, c, x1, x2);
     }
@@ -104,15 +109,14 @@ int Solver(double a, double b, double c, double *x1, double *x2) {
     return n;
 }
 
-int Insert(double *coeff) {
+int Insert(double *coeff, int cnt) {
 
-    printf("# Enter coefficient: ");
-    while (scanf("%lf", coeff) != 1) {
+    printf("# Enter coefficient №%d: ", cnt);
+    while (scanf("%lg", coeff) != 1) {
+        printf("# Error coefficient data type \n");
+        printf("# Enter coefficient №%d with right data type: ", cnt);
         int ch = 0;
-
-        while ()     // getchar()
+        while ((ch = getchar()) != '\n' && ch != EOF) {}
     }
-
-
     return 0;
 }
