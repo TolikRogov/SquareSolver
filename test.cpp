966a40578d8797efed7bd1822fb982a2b0762dc9
TolikRogov <tolik_rogov@bk.ru>
2024-08-20 17:50:58 +0300

    AMC_v2

--- test.cpp
+++ test.cpp
@@ -2,15 +2,34 @@
 #include <math.h>
 
 const int SS_INF_ROOTS = -1;
-const double MIN_VAL = 10e-6;
+const double SS_MIN_VAL = 10e-6;
 
-int check_doubles(double n) {
-    return fabs(n - 0) < MIN_VAL;
+int Solver(double a, double b, double c, double *x1, double *x2);
+
+int main() {
+
+    double a = 0, b = 0, c = 0;
+    double x1 = 0, x2 = 0;
+
+    Insert(&a);
+    Insert(&b);
+
+    //  дальше читать про enum, сделать enum c количество корней
+
+    int nRoots = Solver(a, b, c, &x1, &x2);
+
+    PrintAnswer(nRoots, &x1, &x2);
+
+    return 0;
+}
+
+int NearZero(double n) {
+    return fabs(n) < SS_MIN_VAL;
 }
 
 int SolveLine(double b, double c, double *x1, double *x2) {
-    if (check_doubles(b)) {
-        return (check_doubles(c))? SS_INF_ROOTS : 0;
+    if (NearZero(b)) {
+        return (NearZero(c))? SS_INF_ROOTS : 0;
     }
     else {
         *x1 = -c / b;
@@ -19,7 +38,7 @@ int SolveLine(double b, double c, double *x1, double *x2) {
     }
 }
 
-int GetAnswer(int nRoots, double *x1, double *x2) {
+int PrintAnswer(int nRoots, double *x1, double *x2) {
     switch(nRoots) {
         case 0: {
             printf("No roots \n");
@@ -47,16 +66,21 @@ int GetAnswer(int nRoots, double *x1, double *x2) {
     return 0;
 }
 
-int SolveSquare(double a, double b, double c, double *x1, double *x2) {
-    double d = b * b - 4 * a * c;
+int D(double a, double b, double c) {  // Fix naming
+    return b * b - 4 * a * c;
+}
 
-    if(check_doubles(d)) {
+int SolveSquare(double a, double b, double c, double *x1, double *x2) {
+    double d = D(a, b, c);
+
+    if(NearZero(d)) {
         *x1 = *x2 = -b / ( 2 * a );
         return 1;
     }
     else if(d > 0) {
-        *x1 = ( -b + sqrt(d) ) / (2 * a);
-        *x2 = ( -b - sqrt(d) ) / (2 * a);
+        double q = sqrt(d);
+        *x1 = ( -b + q ) / (2 * a);
+        *x2 = ( -b - q ) / (2 * a);
         return 2;
     }
     return 0;
@@ -64,7 +88,7 @@ int SolveSquare(double a, double b, double c, double *x1, double *x2) {
 
 int Solver(double a, double b, double c, double *x1, double *x2) {
     int n = 0;
-    if (check_doubles(a)) {
+    if (NearZero(a)) {
         n = SolveLine(b, c, x1, x2);
     }
     else {
@@ -73,17 +97,17 @@ int Solver(double a, double b, double c, double *x1, double *x2) {
     return n;
 }
 
-int main() {
-
-    double a = 0, b = 0, c = 0;
-    double x1 = 0, x2 = 0;
+int Insert(double *coeff) {
 
     printf("# Enter number a, b, c: ");
-    scanf("%lg %lg %lg", &a, &b, &c);
+    while (scanf("%lf", coeff) != 1) {
+        int ch = 0;
 
-    int nRoots = Solver(a, b, c, &x1, &x2);
+        while ()     // getchar()
+    }
 
-    GetAnswer(nRoots, &x1, &x2);
 
     return 0;
 }
+
+
